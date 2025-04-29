1d28fdec61ac643666b044215cdd3d2c9d068aad
Nikasha Patel <nikasha.patel@kellogg.northwestern.edu>
2025-04-29 14:19:38 +0000

    completed 1-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_29_135724) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_29_141517) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "city"
     t.string "state"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
